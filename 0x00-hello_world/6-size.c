efaeba7a0d2c6b58c65b2b0648a38250d7de5ed4
Abubeker <abubekrmustefa@gmail.com>
2023-03-20 11:29:47 -0700

    size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  * main - entering start
  *
- Description: C program that prints the size of various types
+ Description: a C program that prints the size of various types
  *
  * Return: Always 0
  */
@@ -12,13 +12,13 @@ int main(void)
 	char character;
 	int intiger;
 	long int l_intiger;
-	long long int ll_int;
+	long long int ll_i;
 	float float_num;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(character));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(intiger));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l_intiger));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(ll_int));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(ll_i));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float_num));
 
 	return (0);
