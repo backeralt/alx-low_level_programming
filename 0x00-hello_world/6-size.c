ae519bdaa7cbe0cac92c22851bac6550e5deb193
Abubeker <abubekrmustefa@gmail.com>
2023-03-20 11:25:15 -0700

    size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /**
  * main - entering start
+ *
  * Description: file size
+ *
  * return: 0
  */
 
@@ -10,13 +12,13 @@ int main(void)
 	char character;
 	int intiger;
 	long int l_intiger;
-	long long int llong_integer;
+	long long int ll_int;
 	float float_num;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(character));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(intiger));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l_intiger));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(llong_integer));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(ll_int));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float_num));
 
 	return (0);
