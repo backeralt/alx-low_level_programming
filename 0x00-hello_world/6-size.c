267d126aa0b1d6f8ad7573f7d0503e0310170638
Abubeker <abubekrmustefa@gmail.com>
2023-03-20 11:07:45 -0700

    size of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	char character;
+	int intiger;
+	long int long_intiger;
+	long long int long_long_integer;
+	float float_num;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(character));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(intiger));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long_intiger));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long_long_integer));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float_num));
+
+	return (0);
+}
